42dce7916ecb58fa8c248251678067b13e8556c4
Luohui-9527 <52158072+Luohui-9527@users.noreply.github.com>
2019-08-19 09:31:38 +0800

    登陆

--- boss-bes-user-permission-service/src/main/java/com/bosssoft/bes/user/permission/service/impl/LoginServiceImpl.java
+++ boss-bes-user-permission-service/src/main/java/com/bosssoft/bes/user/permission/service/impl/LoginServiceImpl.java
@@ -20,6 +20,7 @@ public class LoginServiceImpl implements LoginService {
 
     @Override
     public UserPermission checkUser(UserVO userVO) {
+        System.out.println(loginMapper.checkUser(userVO));
         return loginMapper.checkUser(userVO);
     }
 

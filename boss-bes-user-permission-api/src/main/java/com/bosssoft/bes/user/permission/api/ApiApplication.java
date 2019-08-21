c621ff36653b7f0b12cc8f5d256584e915577fc5
c815852517 <815852517@qq.com>
2019-08-21 14:49:36 +0800

    添加服务向注册中心注册配置

--- boss-bes-user-permission-api/src/main/java/com/bosssoft/bes/user/permission/api/ApiApplication.java
+++ boss-bes-user-permission-api/src/main/java/com/bosssoft/bes/user/permission/api/ApiApplication.java
@@ -2,10 +2,13 @@ package com.bosssoft.bes.user.permission.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @EnableAspectJAutoProxy
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = {"utils","com.bosssoft.hr.train.bossbescommonlogging.annotation", "com.bosssoft.bes.user.permission.service","com.bosssoft.bes.user.permission.**.controller"})
 @MapperScan(basePackages ="com.bosssoft.bes.user.permission.dao" )
 public class ApiApplication {
